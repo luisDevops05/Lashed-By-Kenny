9568d4795e2b7de7f6e58bd792bd886df5538fe0
JlSanchez05 <luisdevops0@gmail.com>
2022-10-23 17:27:57 -0400

    added policys page

---
+++ policies.js
@@ -0,0 +1,2 @@
+// import { allPo, allPolicies,AllPolicesFunction } from "./script";
+
---
+++ script.js
@@ -0,0 +1,23 @@
+// export {allPo,allPolicies,AllPolicesFunction} 
+
+// let depositeLink = document.getElementById('depo')
+// let lateLink = document.getElementById('late')
+// let cancelLink = document.getElementById('cancel') 
+
+// let allPo = document.getElementById('all')
+// let allPolicies = document.getElementById('policiesX1')
+
+
+
+// let AllPolicesFunction = function(){
+// allPolicies.style.display = 'none'
+
+// allPo.addEventListener('click',function(){
+//     allPolicies.style.display = 'block'
+// })
+
+// }
+
+// AllPolicesFunction()
+
+
