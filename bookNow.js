adbb6e34c49d6a595381743013b22e60f52d8bf8
JlSanchez05 <luisdevops0@gmail.com>
2022-11-06 17:57:46 -0500

    working on booking page

--- bookNow.js
+++ bookNow.js
@@ -11,15 +11,24 @@ let nova = document.getElementById('nova')
 let calCon = document.getElementById('calConMain')
 let calCon2 = document.getElementById('calConMain2')
 
+let rvaCalCon = document.getElementById('rvaMainCon')
+let rvaCalCon2 = document.getElementById('rvaMainCon2')
+
+rvaCalCon.style.display='none' 
+rvaCalCon2.style.display='none' 
+
+
+
+
+
+
 
 let alertMessages = function(){
-  
 if (confirm("By pressing ok you are agreeing to have read all terms and policies ") == true) {
     console.log('true');
     alertMsg.style.display='block'
     locationMsg.style.display='block'
     locations.style.display='flex'
-  
 } else {
     alertMsg.style.display='block'
     dynamicP.innerHTML = 'You have not read the terms and policies, please go back and read them as they are strictly enforced. Thank you'
@@ -27,30 +36,39 @@ if (confirm("By pressing ok you are agreeing to have read all terms and policies
 }  
 }
 
-dynamicCalander = function(){
+    let rvaUrl ='https://calendly.com/testingcalen123/30min?background_color=1a1a1a&text_color=ffffff&primary_color=e0afce'
+    
+    let novaUrl ='https://calendly.com/testingcalen123/nova'
+    
+rvaCalCon.setAttribute('data-url',rvaUrl)
+rvaCalCon2.setAttribute('data-url',novaUrl)
 
-rvaBtn.addEventListener('click',function(){
-console.log('rva btn is working')
-
-if(rvaBtn){
-    calCon.style.display='block'
-}
-
-
-})
 
 nova.addEventListener('click',function(){
-    calCon2.style.display='block'
-})
+
+    if(nova = true){
+              rvaCalCon2.style.display='block'
+    }
+
+
+  })
 
 
 
-}
+  rvaBtn.addEventListener('click',function(){
+      console.log('working')
+   
+    
+    if(rvaBtn = true){
+        // console.log('hello')
+           rvaCalCon.style.display='block'
+    
+    }
 
 
+  })
 
-// dynamicCalander()
-
+  
 
 // alertMsg.style.display='block'
 // locationMsg.style.display='block'
